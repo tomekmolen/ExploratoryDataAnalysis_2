3dedede9438ac8ce08b0e644f9650be400bcf48c
tomekmolen <tomekmolen@users.noreply.github.com>
2014-05-26 00:06:38 +0200

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,11 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+NEIsample <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
+
+MD <- subset(NEI, fips=='24510')
+
+png(filename='plot2.png')
+barplot(tapply(X=MD$Emissions, INDEX=MD$year, FUN=sum), 
+                 main='Total Emission in Baltimore City, MD', xlab='Year', ylab=expression('PM'[2.5]))
+dev.off()
